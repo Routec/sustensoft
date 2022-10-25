b0055ee92b7a7beb76f1479e04a7dca27aaa2136
Tiarlei Noeremberg <tiarlei@ymail.com>
2022-10-25 11:06:26 -0300

    Add files via upload
    Criação

---
+++ src/br/com/sustensoft/dao/CadastroUsuarioDAO.java
@@ -0,0 +1,16 @@
+
+package br.com.sustensoft.dao;
+
+import br.com.sustensoft.dto.CadastroUsuarioDTO;
+
+/**
+ *
+ * @author Eletron
+ */
+public class CadastroUsuarioDAO {
+    
+    public void cadastrarUsuario(CadastroUsuarioDTO objCadastroUsuarioDTO){
+        
+    }
+    
+}
---
+++ src/br/com/sustensoft/dto/CadastroUsuarioDTO.java
@@ -0,0 +1,157 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.sustensoft.dto;
+
+/**
+ *
+ * @author Eletron
+ */
+public class CadastroUsuarioDTO {
+    
+    
+
+    private int iduser;
+    private String login, senha, nome, sobrenome, cpf, fone, endereco, cep, email;
+
+    /**
+     * @return the iduser
+     */
+    public int getIduser() {
+        return iduser;
+    }
+
+    /**
+     * @param iduser the iduser to set
+     */
+    public void setIduser(int iduser) {
+        this.iduser = iduser;
+    }
+
+     /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the sobrenome
+     */
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    /**
+     * @param sobrenome the sobrenome to set
+     */
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the fone
+     */
+    public String getFone() {
+        return fone;
+    }
+
+    /**
+     * @param fone the fone to set
+     */
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+     /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
